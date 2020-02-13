d887cf8fc3856f15de8f43f06ec7856f6673975c
NabotKevin <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-02-13 19:26:20 +0100

    scene 1.5 OK

--- DossierV.Final/histoire1.5/histoire1.5.js
+++ DossierV.Final/histoire1.5/histoire1.5.js
@@ -6,32 +6,30 @@ let charaBox = $('#charaBox')
 let numDialBox = 0;
 let chambre = $('#chambre')
 let next = $('#next')
-
-let nbreVictoire = $session.get('nbreVictoire');
-
-if (nbreVictoire == 1) {
-    window.alert('1');
-} else {
-  window.alert('0');
-}
+let sprite1 = $('#sprite1')
+let sprite2 = $('#sprite2')
+let miroir = $('#miroir')
 
 suivant.on("click", function(){
        if (numDialBox == 0) {
           charaBox.css('visibility','hidden');
-          text.html("...");
+          text.html("Votre regard est empli de fierté.");
           numDialBox++;
       }  else if (numDialBox == 1) {
           charaBox.css('visibility','hidden');
           chambre.css('visibility','visible');
-          text.html("Une lumière blanche vous éblouit alors que vous reprenez connaissance. Vous observez la pièce dans laquelle vous vous trouvez, mais vous ne savez pas où vous êtes.");
+          text.html("Ce chaleureux souvenir recouvert, vous sentez des larmes de nostalgie couler sur vos joue, mais étrangement, aucune goutte ne vous apparait.");
           numDialBox++;;
       }  else if (numDialBox == 2) {
           charaBox.css('visibility','hidden');
-          text.html("Vous avez beau essayer, vous n'arrivez pas à vous rappeler. Vous commencez à être effrayé et vous souvenir devient une obsession.");
+          sprite1.css('visibility','hidden');
+          sprite2.css('visibility','hidden');
+          text.html("Vous vous regarder dans le miroir et vous vous apercez que votre corps n'est plus, vous comprenez que vous êtes décédé, mais vous ne savez pas pourquoi ni comment.");
+          miroir.css('visibility','visible');
           numDialBox++;
       } else if (numDialBox == 3) {
           charaBox.css('visibility','hidden');
-          text.html("Vous sortez du lit et vous mettez à chercher dans la chambre des indices qui pourraient vous rafraîchir la mémoire. Un flashback vous apparaît, vous avez l’impression de voir quelqu’un dessiner.");
+          text.html("Vous décidez de sortir de chez vous afin de commencer vos recherches.");
           numDialBox++;
       } else if (numDialBox == 4) {
           dialBox.css('visibility','hidden');
